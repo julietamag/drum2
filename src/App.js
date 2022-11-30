989a2e983c146a382c6b9d3f10f281bbc748fe3b
Julieta Magnago <julietamagnago@gmail.com>
2022-11-30 11:30:55 +0100

    freeCodeCamp version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,112 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
+const drumPads = [
+  {
+    keyCode: 81,
+    keyTrigger: 'Q',
+    id: 'Heater-1',
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+  },
+  {
+    keyCode: 87,
+    keyTrigger: 'W',
+    id: 'Heater-2',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+  },
+  {
+    keyCode: 69,
+    keyTrigger: 'E',
+    id: 'Heater-3',
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+  },
+  {
+    keyCode: 65,
+    keyTrigger: 'A',
+    id: 'Heater-4',
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+  },
+  {
+    keyCode: 83,
+    keyTrigger: 'S',
+    id: 'Clap',
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+  },
+  {
+    keyCode: 68,
+    keyTrigger: 'D',
+    id: 'Apoen-HH',
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+  },
+  {
+    keyCode: 90,
+    keyTrigger: 'Z',
+    id: 'Kick-n-Hat',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+  },
+  {
+    keyCode: 88,
+    keyTrigger: 'X',
+    id: 'Kick',
+    url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+  },
+  {
+    keyCode: 67,
+    keyTrigger: 'C',
+    id: 'Closed-HH',
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+  }
+]
 
 function App() {
+  const [active, setActive] = useState('')
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyPress);
+  }, [])
+
+  function handleKeyPress(event){
+    // if (event.key === drumPads.keyCode) {
+      console.log(event.key)
+      playSound(event.key.toUpperCase())
+    
+  }
+
+  function playSound(selector){
+    const audio = document.getElementById(selector);
+    setActive(selector);
+    // setTimeout((() => setActive(false)), 200);
+    audio.play();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div id='drum-machine'>
+        <div id='drum-pads'>
+          {drumPads.map((drumPad) => (
+            <div 
+            className={`drum-pad ${active}`} 
+            id={drumPad.id}
+            onClick={() => {playSound(drumPad.keyTrigger)}}
+            key={drumPad.id}
+            >
+              {drumPad.keyTrigger}
+              <audio
+              className='clip'
+              id={drumPad.keyTrigger}
+              src={drumPad.url}
+              ></audio>
+              </div>
+          ))}
+        </div>
+        <div id='controller'>
+        <div id="display">
+        <h2>
+          {active}
+        </h2>
+      </div>
+        </div>
+     </div>
     </div>
   );
 }
