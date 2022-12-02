4d42039fdf1d9f11c2d5125bd4fb097d60165533
Julieta Magnago <julietamagnago@gmail.com>
2022-12-02 11:04:52 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,58 +4,67 @@ import React, { useState, useEffect } from 'react';
 
 const drumPads = [
   {
+    borderColor: '#8874BF',
     keyCode: 81,
     keyTrigger: 'Q',
-    id: 'Heater-1',
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+    id: 'Chanchito Hermoso',
+    url: "./resources/audio/chanchito_hermoso.mp3"
   },
   {
+    borderColor: '#6223BD',
     keyCode: 87,
     keyTrigger: 'W',
-    id: 'Heater-2',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+    id: 'OiOiOi',
+    url: './resources/audio/OIoi1.mp3'
   },
   {
+    borderColor:'#5141D1',
     keyCode: 69,
     keyTrigger: 'E',
-    id: 'Heater-3',
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+    id: 'Testraño Porito',
+    url: "./resources/audio/testrañoPorito.mp3"
   },
   {
+    borderColor: '#057AEB',
     keyCode: 65,
     keyTrigger: 'A',
-    id: 'Heater-4',
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+    id: 'Te Full Amo',
+    url: "./resources/audio/teFullAmo.mp3"
   },
   {
+    borderColor: '#0F64C7',
     keyCode: 83,
     keyTrigger: 'S',
-    id: 'Clap',
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+    id: 'chanchuChan',
+    url: "./resources/audio/chanchuChan.mp3"
   },
   {
+    borderColor: '#42758A',
     keyCode: 68,
     keyTrigger: 'D',
-    id: 'Apoen-HH',
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+    id: 'Mimos Infinitos',
+    url: "./resources/audio/mimos_infinitos.mp3"
   },
   {
+    borderColor: '#9FD458',
     keyCode: 90,
     keyTrigger: 'Z',
-    id: 'Kick-n-Hat',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+    id: 'Vampira P',
+    url: './resources/audio/vampira_puta.mp3'
   },
   {
+    borderColor: '#5ED88E',
     keyCode: 88,
     keyTrigger: 'X',
-    id: 'Kick',
-    url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+    id: 'puPi',
+    url: "./resources/audio/puPi.mp3"
   },
   {
+    borderColor: '#85DCC2',
     keyCode: 67,
     keyTrigger: 'C',
-    id: 'Closed-HH',
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+    id: 'Quien es mi bb',
+    url: "./resources/audio/quien_es_mibb.mp3"
   }
 ]
 
